9382466ae47ba32dca34a6221b6de1f0b8af3b52
Sean O'Daniels <sodaniels@gmail.com>
2023-11-15 16:29:45 -0500

    Solution for exercise 3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const morgan = require('morgan')
 
 app.use(express.json())
 
-app.use(morgan('tiny'))
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body ', {
+  skip: function (req, res) { return req.method != "POST" }
+}))
+
+app.use(morgan('tiny', {
+  skip: function (req, res) { return req.method == "POST" }
+}))
 
 let persons = [
   {
@@ -64,7 +74,7 @@ let persons = [
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
 
-    response.status(204).end() 
+    response.status(204).end()
   })
 
   const generateId = () => {
@@ -98,7 +108,6 @@ let persons = [
         error: `${body.name} already exists`
       })
     }
-
   })
 
 const PORT = 3001
