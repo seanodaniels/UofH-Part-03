528ba50e09575cc24296f364b1ef775299e2e95b
Sean O'Daniels <sodaniels@gmail.com>
2023-11-21 15:32:08 -0500

    Solution for exercise 3.15: Phonebook database, step3. Delete functionality.

--- index.js
+++ index.js
@@ -28,16 +28,20 @@ app.use(morgan('tiny', {
 //
 // Routes
 //
+  
+  // Default root url
   app.get('/', (request, response) => {
     response.send('<h1>Express Server Status</h1><p>Online. <a href="/api/persons">/api/persons</a> for contents.')
   })
 
+  // GET entire DB
   app.get('/api/persons', (request, response) => {
     Person.find({}).then(person => {
       response.json(person)
     })
   })
 
+  // Get single entry
   app.get('/api/persons/:id', (request, response) => {
     Person.findById(request.params.id).then(person => {
       response.json(person)
@@ -53,11 +57,12 @@ app.use(morgan('tiny', {
 
   })
 
+  // DELETE single entry
   app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
   })
 
   // Save new entry to MongoDB
@@ -90,8 +95,11 @@ app.use(morgan('tiny', {
       })
     }
   })
+//
+// END Routes
+//
 
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
