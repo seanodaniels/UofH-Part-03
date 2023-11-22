7283afd0e1dab237c17ef0ecbb7671eed8fe10fc
Sean O'Daniels <sodaniels@gmail.com>
2023-11-22 10:54:01 -0500

    Solution for exercise 3.20*: Phonebook database, step8. Removed front end validation for missing name or number and added that validation on the backend.

--- models/person.js
+++ models/person.js
@@ -20,7 +20,16 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{1,}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number.`
+    },
+    required: [true, 'User phone number required']
+  },
 })
 
 // Transform the default DB format to whatever we want it to be.
